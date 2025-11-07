cbb69321f86feebb8c322a7d558f1dacf3e60484
Pushpendra960 <whitedevil08092006@gmail.com>
2025-11-07 20:08:33 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,3 +1,16 @@
- #include <stdio.h>
- int main()
-    
+#include <stdio.h>
+
+int fact(int n);
+
+int main() {
+    int num = 5;
+    printf("Factorial of %d = %d", num, fact(num));
+    return 0;
+}
+
+// Very simple recursive factorial
+int fact(int n) {
+    if (n == 0) 
+        return 1;
+    return n * fact(n - 1);
+}
