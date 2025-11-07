0e5914fefd4c926ffce58b0661247d6295e1dc6f
Pushpendra960 <whitedevil08092006@gmail.com>
2025-11-07 20:04:10 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-int main ()
-{
-    int n;
-    printf("Enter a number :- ");
-    scanf("%d" , &n);
 
-    int a = -1, b = 1 , c;
-
-    for (int i=1; i <= n; i++)
-    {
-        c = a+b;
-        a=b;
-        b=c;
-        printf("%d" , c);
+int main() {
+    int n, i;
+    int first = 0, second = 1, next;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for(i = 0; i < n; i++) {
+        if(i <= 1) {
+            next = i;  // First two terms are 0 and 1
+        } else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d ", next);
     }
-    
-    printf("\n");
     return 0;
 }
